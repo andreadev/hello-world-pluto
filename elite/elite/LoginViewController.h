e04ade77661e6c351b310b386c0c8c29456f5155
andrea <andrea@imac-di-giacomo.homenet.telecomitalia.it>
2013-03-06 10:44:45 +0100

    add-loginviewcontroller

---
+++ elite/elite/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  elite
+//
+//  Created by Andrea on 06/03/13.
+//  Copyright (c) 2013 Andrea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
