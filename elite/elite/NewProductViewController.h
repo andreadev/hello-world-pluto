362ca677ef0ae222e53d19c99a4dc7871bed8d93
Andrea Barbieri <andrea.barbieri1991@gmail.com>
2013-03-21 10:59:05 +0100

    Revert "new"
    This reverts commit c62008caae5124cbfdf3054733b25b80430d4b2d.

--- elite/elite/NewProductViewController.h
+++ elite/elite/NewProductViewController.h
@@ -10,13 +10,5 @@
 
 
 @interface NewProductViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UIImageView *imageProd;
-@property (weak, nonatomic) IBOutlet UITextField *nameProd;
-@property (weak, nonatomic) IBOutlet UITextField *priceProd;
-@property (weak, nonatomic) IBOutlet UITextField *categoryProd;
-@property (weak, nonatomic) IBOutlet UITextField *shopProd;
-@property (weak, nonatomic) IBOutlet UITextField *descProd;
-- (IBAction)photo:(id)sender;
 
-- (IBAction)consiglia:(id)sender;
 @end
