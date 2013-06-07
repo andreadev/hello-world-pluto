34577392a2a7f84a85d1cf7bf8f45627751117c0
Andrea Barbieri <andrea.barbieri1991@gmail.com>
2013-06-07 08:41:10 +0200

    New SDK facebook new load prod!

---
+++ elite/elite/LoadDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoadDetailViewController.h
+//  elite
+//
+//  Created by Andrea Barbieri on 05/06/13.
+//  Copyright (c) 2013 Andrea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadDetailViewController : UIViewController
+
+@end
